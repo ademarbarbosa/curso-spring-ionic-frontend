9d142f9c3e2cb362d6eecbdfced2bb6fe216df58
ademarbarbosa <ademar.barbosa@gmail.com>
2018-04-11 18:40:16 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -13,7 +13,9 @@ export class ProdutosPage {
 
   items : ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService : ProdutoService) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public produtoService : ProdutoService) {
   }
 
   ionViewDidLoad() {
@@ -21,7 +23,20 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
-  };
+  }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
+  }  
+
 }
