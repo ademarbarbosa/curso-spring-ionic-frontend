2562032c3683038d85cddc30ec4491a56d615eb5
ademarbarbosa <ademar.barbosa@gmail.com>
2018-05-13 19:58:38 -0300

    Salvando o pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -19,7 +20,13 @@ export class OrderConfirmationPage {
   cliente : ClienteDTO;
   endereco : EnderecoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public cartService : CartService, public clienteService : ClienteService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public cartService : CartService, 
+    public clienteService : ClienteService, 
+    public pedidoService : PedidoService) {
+
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -43,6 +50,23 @@ export class OrderConfirmationPage {
 
   total() {
     return this.cartService.total();
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
   }
 
 }
