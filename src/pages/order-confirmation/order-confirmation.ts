89333e131237fb8fb99908eb4812d1ade36544f7
ademarbarbosa <ademar.barbosa@gmail.com>
2018-05-14 19:31:47 -0300

    Informando o pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems : CartItem[];
   cliente : ClienteDTO;
   endereco : EnderecoDTO;
+  codPedido : string;
 
   constructor(
     public navCtrl: NavController, 
@@ -60,13 +61,22 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403) {
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
